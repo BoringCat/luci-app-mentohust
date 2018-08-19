45bb35e3031e39bfa7d40a7cb7c5b09c11f10455
boringcat <c654477757@gmail.com>
2018-08-19 16:39:42 +0800

    Fix a BUG which can write log to config file

--- files/luci/model/cbi/mentohust/log.lua
+++ files/luci/model/cbi/mentohust/log.lua
@@ -1,17 +1,23 @@
-require("luci.sys")
+local fs = require "nixio.fs"
 
-m = Map("mentohust", translate("MentoHUST LOG"), translate("Log file:/tmp/mentohust.log"))
+local f = SimpleForm("mentohust",
+	translate("MentoHUST LOG"),
+	translate("Log file:/tmp/mentohust.log"))
 
-s = m:section(TypedSection, "mentohust", "")
-s.addremove = false
-s.anonymous = true
+local o = f:field(Value, "mentohust_log")
 
-view_cfg = s:option(TextValue, "1", nil)
-    view_cfg.rows = 25
-    view_cfg.readonly = true
-  
-    function view_cfg.cfgvalue()  
-        return nixio.fs.readfile("/tmp/mentohust.log") or ""  
-    end  
+o.template = "cbi/tvalue"
+o.rows = 32
 
-return m
+function o.cfgvalue(self, section)
+	return fs.readfile("/tmp/mentohust.log")
+end
+
+function o.write(self, section, value)
+	require("luci.sys").call('cat /dev/null > /tmp/mentohust.log 2>/dev/null')
+end
+
+f.submit = translate("Clear log")
+f.reset = false
+
+return f
